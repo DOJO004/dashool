0f6b0e4d4fc7779d40c90d8a22b217b96be4f870
DOJO <kevin11272@gmail.com>
2024-03-25 16:30:52 +0800

    add base_wine column to cocktail and add category search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @alcohol_category = %w[whisky gin brandy]
-    @classic_cocktails = Cocktail.where(type_of: "Classic")
+    @alcohol_category = %w[Whisky Gin Brandy Vodka Rum Tequila]
+    if (@alcohol_category & [params[:base_wine]]).present?
+      @classic_cocktails = Cocktail.where(base_wine: params[:base_wine])
+    else
+      @classic_cocktails = Cocktail.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
 
+  get "?base_wine=:base_wine", to: "pages#home", as: "filter_base_wine"
+
   resources :cocktails
 end
