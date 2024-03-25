01c6dc92acf0325b65d1ce9d09f9f9d7dd1e49e8
DOJO <kevin11272@gmail.com>
2024-03-25 14:49:41 +0800

    add cocktail edit page and delete product_method column from cocktail

--- db/migrate/20240322133619_add_columns_to_cocktail.rb
+++ db/migrate/20240322133619_add_columns_to_cocktail.rb
@@ -1,7 +1,6 @@
 class AddColumnsToCocktail < ActiveRecord::Migration[7.1]
   def change
     add_column :cocktails, :history, :text
-    add_column :cocktails, :production_method, :text
     add_column :cocktails, :drink_style, :string
     add_column :cocktails, :skill, :string
   end
