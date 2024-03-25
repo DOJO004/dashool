774c300f8d3617da9755e1a67cf1c6668fc7bfdd
DOJO <kevin11272@gmail.com>
2024-03-25 14:52:48 +0800

    run rubo cop

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Dashool
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -27,6 +27,5 @@ module Dashool
     config.generators do |generate|
       generate.orm :active_record, primary_key_type: :uuid
     end
-    
   end
 end
