dcbe1105129a70aae58c1c1057833e49d7601dd2
DOJO <kevin11272@gmail.com>
2024-03-25 16:31:38 +0800

    run rubocop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def home
     @alcohol_category = %w[Whisky Gin Brandy Vodka Rum Tequila]
-    if (@alcohol_category & [params[:base_wine]]).present?
-      @classic_cocktails = Cocktail.where(base_wine: params[:base_wine])
-    else
-      @classic_cocktails = Cocktail.all
-    end
+    @classic_cocktails = if (@alcohol_category & [params[:base_wine]]).present?
+                           Cocktail.where(base_wine: params[:base_wine])
+                         else
+                           Cocktail.all
+                         end
   end
 end
