3317bc285dec66c53ba626f18adceb3680f77904
DOJO <kevin11272@gmail.com>
2024-04-03 13:53:05 +0800

    add google map to cocktail map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,5 @@
 class PagesController < ApplicationController
-    def cocktail_map
-        api_key = "AIzaSyBlVjjT_fjwhZCdnX4Z3bmQ6RpGgy4aphU"
-        location = "25.0330,121.5654" # 這裡應該是客戶端傳遞的位置，你可以通過 params[:latitude] 和 params[:longitude] 來獲取
-        radius = 5000
-        url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{api_key}&location=#{location}&radius=#{radius}&keyword=bar")
-        response = Net::HTTP.get(url)
-        puts response
-        @data = JSON.parse(response)
-    end
+  def cocktail_map
+    api_key = "AIzaSyBlVjjT_fjwhZCdnX4Z3bmQ6RpGgy4aphU"
+  end
 end
