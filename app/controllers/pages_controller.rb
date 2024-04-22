381ece56c19f6277faeb0fb1ceeb98d24572bd40
DOJO <kevin11272@gmail.com>
2024-04-22 17:11:48 +0800

    fix search page search value = nil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,13 @@ class PagesController < ApplicationController
 
   def search
     search_value = params[:search_value]
-    cocktails = Cocktail.where("LOWER(name) LIKE ?", "%#{search_value.downcase}%").order(name: :asc)
-    classic_cocktails = ClassicCocktail.where("LOWER(name) LIKE ?", "%#{search_value.downcase}%").order(name: :asc)
+    if search_value.nil?
+      cocktails = Cocktail.all
+      classic_cocktails = ClassicCocktail.all
+    else
+      cocktails = Cocktail.where("LOWER(name) LIKE ?", "%#{search_value.downcase}%").order(name: :asc)
+      classic_cocktails = ClassicCocktail.where("LOWER(name) LIKE ?", "%#{search_value.downcase}%").order(name: :asc)
+    end
 
     @data = cocktails + classic_cocktails
   end
