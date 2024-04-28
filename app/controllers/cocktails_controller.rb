f8fed96e57163a5cee1755c119d904da0d0d302f
DOJO <kevin11272@gmail.com>
2024-04-28 14:04:54 +0800

    add upload image size 500kb

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require "image_processing/mini_magick"
 
 class CocktailsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
@@ -20,20 +21,30 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = current_user.cocktails.build(cocktail_params)
 
+    if params[:cocktail][:images].present?
+      params[:cocktail][:images].each do |image|
+        next if image.blank?  
+        ok_image = resize_image(image, 1920, 1080)
+        puts "** #{File.size(ok_image.path)}"
+      end
+    end
+    
     if @cocktail.save
       redirect_to @cocktail, notice: "Cocktail was successfully created."
     else
-      flash.now[:alert] = "Cocktail was not created."
+      flash.now[:alert] = @cocktail.errors.full_messages
       render :new
     end
   end
 
   def edit; end
-
+  
   def update
+
     if @cocktail.update(cocktail_params)
       redirect_to cocktail_path(params[:id])
     else
+      flash.now[:alert] = @cocktail.errors.full_messages
       render :edit
     end
   end
@@ -51,5 +62,10 @@ class CocktailsController < ApplicationController
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:id])
+  end
+
+  def resize_image(image, max_width, max_height)
+    pipeline = ImageProcessing::MiniMagick.source(image)
+    pipeline.resize_to_limit!(max_width, max_height)
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,14 +2,29 @@ class Cocktail < ApplicationRecord
   has_rich_text :introduce
   has_many_attached :images
   has_many :likes
-
   belongs_to :user
+  validate :valid_image
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[name base_wine]
   end
 
   def self.ransackable_associations(_auth_object = nil)
-    []
+    []  
+  end
+
+
+  def valid_image
+    return unless images.attached?
+    images.each do |image|
+
+      unless %w[image/jpeg image/png].include?(image.blob.content_type)
+        errors.add(:images, "'#{image.filename}' must be a JPG or PNG file")
+      end
+  
+      if image.blob.byte_size > 500.kilobyte
+        errors.add(:images, " '#{image.filename}' is more than 500KB")
+      end
+    end
   end
 end
