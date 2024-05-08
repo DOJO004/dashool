82cc8b499f2acfda300e6a7d78e324c0229efd40
DOJO <kevin11272@gmail.com>
2024-05-08 16:20:28 +0800

    add svg icons

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one :profile
   has_many :cocktails
   has_many :likes
-
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
