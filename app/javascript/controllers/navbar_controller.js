4b75c5fbc506926e3d5252d56269bfd337ee7c60
DOJO <kevin11272@gmail.com>
2024-06-15 09:21:56 +0800

    change navbar style and add link

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -9,19 +9,19 @@ export default class extends Controller {
   showMenu() {
     const menu = this.menuTarget;
 
-    if (menu.classList.contains('h-0')) {
-      menu.classList.add('h-96');
-      menu.classList.remove('h-0');
+    if (menu.classList.contains('w-0')) {
+      menu.classList.add('w-full');
+      menu.classList.remove('w-0');
     } else {
-      menu.classList.remove('h-96');
-      menu.classList.add('h-0');
+      menu.classList.remove('w-full');
+      menu.classList.add('w-0');
     }
   }
 
   closeMenu() {
     const menu = this.menuTarget;
 
-    menu.classList.add('h-0');
-    menu.classList.remove('h-96');
+    menu.classList.add('w-0');
+    menu.classList.remove('w-full');
   }
 }
